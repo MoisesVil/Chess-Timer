b08546736041074666a26fdec6b04ea3cad9eee3
ddavis-4 <123651199+ddavis-4@users.noreply.github.com>
2025-09-27 19:58:22 -0500

    get fucked I hope we have conflicts

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: include every folder that contains Nativewind/Tailwind classes
+  // come back and make sure this is what we want.
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
